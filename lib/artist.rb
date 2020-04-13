8e197edea232f4c7586b420bf83e5b46ece2b012
taroj khan <tarujkhan@yahoo.com>
2020-04-13 17:21:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,55 +1,55 @@
-class Artist 
-  
-  extend Concerns::Findable
-  attr_accessor :name
-  attr_reader :songs
-  @@all = []
-  
-  def initialize(name)
-    @name = name 
-    @songs = []
+  class Artist 
     
-end 
-
-def self.all
-  @@all
-end 
-
-def self.destroy_all
-  @@all.clear
-end 
-
-def save
-  self.class.all << self 
-end 
-
-def self.create(name)
-  created_artist = self.new(name)
-  created_artist.save
-  created_artist
+    extend Concerns::Findable
+    attr_accessor :name
+    attr_reader :songs
+    @@all = []
+    
+    def initialize(name)
+      @name = name 
+      @songs = []
+      
   end 
-
- 
- def add_song(song)
-   if !song.artist 
-     song.artist = self 
-   end 
-   if !@songs.include?(song)
-       @songs << song
- end
- 
-end 
-
-def genres
-  @new_array = []
-  @songs.each do |song|
-  if @new_array.include?(song.genre)
-    nil 
-  else 
-    @new_array << song.genre
+  
+  def self.all
+    @@all
   end 
-end 
-    @new_array
-end 
-
-end 
+  
+  def self.destroy_all
+    @@all.clear
+  end 
+  
+  def save
+    self.class.all << self 
+  end 
+  
+  def self.create(name)
+    created_artist = self.new(name)
+    created_artist.save
+    created_artist
+    end 
+  
+   
+   def add_song(song)
+     if !song.artist 
+       song.artist = self 
+     end 
+     if !@songs.include?(song)
+         @songs << song
+   end
+   
+  end 
+  
+  def genres
+    @new_array = []
+    @songs.each do |song|
+    if @new_array.include?(song.genre)
+      nil 
+    else 
+      @new_array << song.genre
+    end 
+  end 
+      @new_array
+  end 
+  
+  end 
